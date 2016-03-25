546dc8337dcc14199e501c693f287996fc52739d
manf88 <gruenesgift@gmx.de>
2016-03-25 21:29:03 +0100

    minor changes

---
+++ UserControls.NotificationWindow/Contracts/IPanel.cs
@@ -0,0 +1,13 @@
+﻿namespace UserControls.NotificationPanel
+{ 
+    public interface IPanel
+    {
+        int MaxNotifications { get; set; }
+
+        void AddNotification(string header, string message);
+
+        void Activate();
+
+        void Deactivate();
+    }
+}
